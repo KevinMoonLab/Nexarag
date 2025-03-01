b09c9caf6f36d2988aa8032c4e1d5977e4c68964
ben <ben.fuller@conexusinc.com>
2025-03-01 16:16:41 -0700

    unused logging

--- backend/src/db/chat.py
+++ backend/src/db/chat.py
@@ -1,13 +1,9 @@
-from db.models import Chat, ChatResponse, ChatMessage
+from db.models import ChatResponse, ChatMessage
 from rabbit.schemas import ChatMessage as RabbitChatMessage, ChatResponse as RabbitChatResponse
-import logging
-
-logger = logging.getLogger(__name__)
 
 async def create_chat_message(message: RabbitChatMessage):
     message = await ChatMessage(message_id=message.messageId, message=message.message).save()
     return message    
-
 
 async def update_chat_response(response: RabbitChatResponse):
     chat_message = await ChatMessage.nodes.get_or_none(message_id=response.userMessageId)
